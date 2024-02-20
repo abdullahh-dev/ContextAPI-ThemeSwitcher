4545cca4fa718b6ba6f3da0c5901e117e2b4ae65
Abdullah <abdullahaha101@gmail.com>
2024-02-20 22:01:37 +0500

    modified card

--- themeSwitch/src/Components/ToggleBtn.jsx
+++ themeSwitch/src/Components/ToggleBtn.jsx
@@ -18,8 +18,8 @@ function ToggleBtn() {
         checked={themeMode === 'dark'}
         onChange={onChangeBtn}
       />
-      <div className="w-11 h-6 bg-gray-200 shadow-sm shadow-[#2d2d2d43]  rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute dark:border dark:border-[hsla(0,0%,100%,.15)] after:top-[2px] after:left-[2px] after:bg-[white]  after:border after:rounded-full after:h-5 after:w-5 after:transition-all duration-300  peer-checked:bg-[#212121] "></div>
-      <span className="ml-3 text-sm font-medium text-[#212121] dark:text-[]">
+      <div className="w-11 h-6 bg-gray-200 shadow-sm shadow-[#2d2d2d43] rounded-full peer peer-checked:after:translate-x-full  after:absolute dark:border dark:border-[hsla(0,0%,100%,.15)] after:top-[2px] after:left-[2px] after:bg-[white]  after:border after:rounded-full after:h-5 after:w-5 after:transition-all duration-500  peer-checked:bg-[#212121] "></div>
+      <span className="ml-3 text-sm font-medium text-[#212121] dark:text-[white]/70">
         {themeMode === 'dark' ? <MdOutlineDarkMode /> : <MdOutlineLightMode />}
       </span>
     </label>
